cd2df12dc67dd2c83e4490da1e138ced573f93b1
darshanrp96 <darshanpardeshi8@gmail.com>
2025-08-27 05:13:26 +0000

    datatypes changes maded

--- basics/datatypes.js
+++ basics/datatypes.js
@@ -1,3 +1,14 @@
-console.log("datatypes");
+console.log("datatypes session");
 
+let n = 10
+console.log(typeof(n));
 
+let s = "darshan"
+console.log(typeof(s));
+
+let b = false
+console.log(typeof(b));
+
+let st=null
+console.log(typeof(st));
+//null datatypes is ---> object
