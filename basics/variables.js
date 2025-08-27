2031c3bb6b501767858052846e11927164869b91
darshanrp96 <darshanpardeshi8@gmail.com>
2025-08-27 04:55:56 +0000

    varibles defination with notes

--- basics/variables.js
+++ basics/variables.js
@@ -1,4 +1,4 @@
-let x=10
+/*let x=10
 const d= 7
 
 let xe=898
@@ -6,5 +6,36 @@ let xe=898
 console.log(x)
 console.log(d)
 console.log(xe)
+
+console.log(); */
+
+// let variables= 
+//console.log(x);
+let x = 300
+console.log(x);
+//blocked socped stating error-Cannot access 'x' before initialization
+
+//var variables
+console.log(y);
+var y =500
+console.log(y);
+//global scope it will take the value before intilisation as undefined
+
+//const variables
+const z= 700
+console.log(z);
+
+//z = 445
+console.log(z);
+// const means it will dfined the fixed value cannot change the value 
+// will throw errro=Assignment to constant variable.
+
+
+
+
+
+
+
+
 
 
