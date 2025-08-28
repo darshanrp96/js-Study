2277066f99e32e0769fedcbda11e6ed7edec5582
darshanrp96 <darshanpardeshi8@gmail.com>
2025-08-28 07:20:46 +0000

    conditinal statememts changss

--- basics/conditionalSt.js
+++ basics/conditionalSt.js
@@ -1,2 +1,16 @@
 console.log("conditional statements");
 
+let age=11
+
+if(age>=18)
+{
+console.log("eligible for votes");
+}
+else
+{
+console.log("NOT eligible for votes");
+}
+
+console.log("exited");
+
+
