d8ce23fa576c623d49be72d1f04ee90b657e621d
darshanrp96 <darshanpardeshi8@gmail.com>
2025-08-28 13:12:31 +0000

    day in week added

--- basics/conditionalSt.js
+++ basics/conditionalSt.js
@@ -30,3 +30,39 @@ else
     console.log("c is greater");
 }
 
+// day in week find
+
+let day=9;
+
+if (day==1) {
+    console.log("day is monday");
+    
+}
+else if (day==2) {
+    console.log("day is tuesday");
+    
+}
+else if (day==3) {
+    console.log("day is wedenesday");
+    
+}
+else if (day==4) {
+    console.log("day is thursday");
+    
+}
+else if (day==5) {
+    console.log("day is friday");
+    
+}
+else if (day==6) {
+    console.log("day is saturday");
+    
+}
+else if (day==7) {
+    console.log("day is sunday");
+    
+}
+else{
+    console.log("day is INVALID");
+    
+}
