ecfe2b2748e3fbe45431e1da023677c4841516a6
darshanrp96 <darshanpardeshi8@gmail.com>
2025-08-28 07:34:07 +0000

    gretaer nu logic added

--- basics/conditionalSt.js
+++ basics/conditionalSt.js
@@ -1,5 +1,5 @@
 console.log("conditional statements");
-
+// if else statement
 let age=11
 
 if(age>=18)
@@ -13,4 +13,20 @@ console.log("NOT eligible for votes");
 
 console.log("exited");
 
+// greatest numbers find logic
+
+let a=10,b=200,c=30;
+
+if (a>b && a>c)
+{
+    console.log("a is greater");
+}
+else if (b>a && b>c)
+{
+    console.log("b is greater");  
+}
+else
+{
+    console.log("c is greater");
+}
 
