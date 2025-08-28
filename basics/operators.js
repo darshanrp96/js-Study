89f46a28249a290b417ba247da41f9de8e583d2b
darshanrp96 <darshanpardeshi8@gmail.com>
2025-08-28 06:09:54 +0000

    operators session changes

---
+++ basics/operators.js
@@ -0,0 +1,32 @@
+console.log("operators");
+
+let x=10,y=20
+
+//arithmatic operators:-
+
+console.log(x+y);
+console.log(x-y);
+console.log(x*y);
+console.log(x%y);
+console.log(x**y);
+
+x=10
+x++;
+//++x;
+
+console.log(x);
+y=20
+y++;
+//++y;
+
+console.log(y);
+
+//relational operators:
+x=12;
+y=22;
+
+
+console.log(x>y);
+console.log(x<y);
+
+
