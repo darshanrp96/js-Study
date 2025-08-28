68378ecbe69ab11b6f48b58d271e2cd63bac2b56
darshanrp96 <darshanpardeshi8@gmail.com>
2025-08-28 16:37:01 +0000

    loop updated

--- basics/loop.js
+++ basics/loop.js
@@ -1,9 +1,28 @@
 console.log("looping statements");
 
+// numbers printing 1-10
 let i= 1;
 
 while(i<=10)
     {
         console.log("value is "+i);
         i++;
+    }
+
+// even number printing
+
+let n=2;
+while(n<=10)
+    {
+        console.log("value is "+n);
+        n+=2;
+    }
+
+// decending order no
+
+let d = 10;
+while(d>0)
+    {
+        console.log("value is "+d);
+        d--;
     }
