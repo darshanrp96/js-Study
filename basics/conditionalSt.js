5189efe2693b58e251b737cd60aa68c780f419aa
darshanrp96 <darshanpardeshi8@gmail.com>
2025-08-28 13:21:08 +0000

    switch case added

--- basics/conditionalSt.js
+++ basics/conditionalSt.js
@@ -66,3 +66,19 @@ else{
     console.log("day is INVALID");
     
 }
+
+//swtich case :
+
+let d=1;
+
+switch(d)
+{
+    case 1: console.log("monday"); break;
+    case 2: console.log("tuesday"); break;
+    case 3: console.log("wednesday"); break;
+    case 4: console.log("thursady"); break;
+    case 5: console.log("friday"); break;
+    case 6: console.log("saturday"); break;
+    case 7: console.log("sunday"); break;
+    default: console.log("INVALID"); 
+}
